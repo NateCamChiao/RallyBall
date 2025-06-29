4c41050913dc88fd0ed36e0097d788cc95399a59
Nate <142466177+NateCamChiao@users.noreply.github.com>
2025-06-29 02:39:37 -0700

    Major commit changing RenderState constructor This now passes in Client Rendering Data from constants as well as playerstate to figure out animation data and positioning data. Removed class members that were redundant.

--- scripts/client-side/clientPlayer.ts
+++ scripts/client-side/clientPlayer.ts
@@ -1,84 +1,22 @@
-import { ANIMATION_DETAILS, DIRECTION, PLAYER_ANIMATION, Coordinates, PlayerStates, POSITION_FUNCTIONS, CLIENT_RENDERING } from "./constants.js";
+import { ANIMATION_DETAILS, DIRECTION, PLAYER_ANIMATION, Coordinates, PlayerStates, POSITION_FUNCTIONS, CLIENT_RENDERING, ClientRenderData } from "./constants.js";
 
 
 
 type CoordConversionFn = (x: number, y: number) => Coordinates;
 class RenderState{
-    mapRow: number;
-    freezeFrame: number;
-    fps: number;
-    maxFrames: number;
     initialPosition: Coordinates;
     startDate: number;
     serverToClientCoords: CoordConversionFn;
     playerState: PlayerStates;
-    renderingData: any;
-    constructor(spriteMapRow: number, 
-        framesPerSecond: number, 
-        animationFrameLength: number, 
-        initialPosition: Coordinates, 
-        coordConversion: CoordConversionFn, 
-        freezeFrame = 0, 
-        startDate = Date.now(),
-        playerState = PlayerStates.Idle){
-        this.mapRow = spriteMapRow;
-        this.freezeFrame = freezeFrame;
-        this.fps = 1000 / framesPerSecond; //frame length
-        this.maxFrames = animationFrameLength; // inclusive
-        this.initialPosition = initialPosition;
-        this.startDate = startDate;
+    renderData: ClientRenderData;
+    constructor(renderData: ClientRenderData, initalPosition: Coordinates, coordConversion: CoordConversionFn, playerState = PlayerStates.Idle, startDate = Date.now()){
+        this.renderData = renderData;
+        this.initialPosition = initalPosition;
+        this.serverToClientCoords = coordConversion;
         this.playerState = playerState;
-        //method
-        this.serverToClientCoords = coordConversion;
-    }
-    /**
-     * @deprecated
-     * @param spriteMapRow 
-     * @param framesPerSecond 
-     * @param animationFrameLength 
-     * @param freezeFrame 
-     * @returns 
-     */
-    addAnimationInfo(spriteMapRow: number, framesPerSecond: number, animationFrameLength: number, freezeFrame = 0){
-        this.mapRow = spriteMapRow;
-        this.fps = framesPerSecond;
-        this.maxFrames = animationFrameLength;
-        this.freezeFrame = freezeFrame;
-        return this;
-    }
-    /**
-     * @deprecated
-     * @param initialPosition 
-     * @param coordConversion 
-     * @param startDate 
-     * @returns 
-     */
-    addCoordInfo(initialPosition: Coordinates, coordConversion: CoordConversionFn, startDate = Date.now()){
-        this.initialPosition = initialPosition;
-        this.serverToClientCoords = coordConversion;
         this.startDate = startDate;
-        return this;
     }
 
-    /**
-     * Alternate constructor if creating RenderState by passing in the cooresponding ANIMATION_DETAILS
-     */
-
-    static createWithAnimationDetails(animationDetails: any, initialPosition: Coordinates, coordConversion: CoordConversionFn, startDate = Date.now()){
-        return new RenderState(
-            animationDetails.animation.mapRow,
-            animationDetails.animation.fps,
-            animationDetails.animation.maxFrame,
-            initialPosition,
-            coordConversion,
-            animationDetails.freezeFrame ?? 0,
-            startDate,
-            animationDetails
-        );
-    }
-    addClientRenderingData(clientRenderingData: any){
-        this.renderingData = clientRenderingData;
-    }
     //client-side prediction
     calculateClientCoords(direction: DIRECTION): Coordinates{
         let {x, y} = this.initialPosition;
@@ -86,19 +24,21 @@ class RenderState{
         
         //todo plug into fn(initialPos, t)
         // console.log(PHYSICS_FUNCTIONS[this.playerState](this.initialPosition, direction, timeElapsed));
-
-
         return this.serverToClientCoords(x, y);
     }
 
     render(ctx: CanvasRenderingContext2D, deltatime: number, direction: DIRECTION, size: number, spriteMap: CanvasImageSource){
+        let { maxFrame, mapRow, fps, freezeFrame } = this.renderData[this.playerState].animation;
+        //converts to frame length
+        let frameLength = 1000 / fps;
         let animationLength = Date.now() - this.startDate;
-        let frame = Math.floor(animationLength / this.fps) % (this.maxFrames); // * (delta time) /  mod (maxFrames * )
+        let frame: number = Math.floor(animationLength / frameLength) % (maxFrame); // * (delta time) /  mod (maxFrames * )
         // If can't divide by fps then use freezeFrame
-        if(this.fps == Infinity || this.fps == 0){
-            frame = this.freezeFrame;
+        if(frameLength == Infinity || frameLength == 0){
+            //zero if freezeFrame isn't available
+            frame = freezeFrame ?? 0;
         }
-        console.log(frame, this.fps)
+        console.log(frame, frameLength)
         let coords = this.calculateClientCoords(direction);
         ctx.save();
         
@@ -109,7 +49,7 @@ class RenderState{
         ctx.drawImage(
             spriteMap, 
             frame * PLAYER_ANIMATION.FRAME_WIDTH,
-            this.mapRow * PLAYER_ANIMATION.FRAME_WIDTH,
+            mapRow * PLAYER_ANIMATION.FRAME_WIDTH,
             PLAYER_ANIMATION.FRAME_WIDTH,
             PLAYER_ANIMATION.FRAME_WIDTH,
             -size / 2,
@@ -138,7 +78,7 @@ export class PlayerRenderer{
     spriteMap: any;
     constructor(ctx: any, dir: number, initialPosition: { x: number; y: number; }, startDate: Date, spriteMap: any, coordConvertingFunction: CoordConversionFn){
         this.ctx = ctx;
-        this.renderState = RenderState.createWithAnimationDetails(CLIENT_RENDERING.Idle, initialPosition, coordConvertingFunction);
+        this.renderState = new RenderState(CLIENT_RENDERING, initialPosition, coordConvertingFunction, PlayerStates.Idle);
         this.dir = dir;
         this.initialPosition = initialPosition;
         this.startDate = startDate;
