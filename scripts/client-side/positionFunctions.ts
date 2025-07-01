6080edd3fc653764077e4576fbb6b05bbf869254
Nate <142466177+NateCamChiao@users.noreply.github.com>
2025-07-01 16:35:05 -0700

    PositionFunction temporary coord fix

--- scripts/client-side/positionFunctions.ts
+++ scripts/client-side/positionFunctions.ts
@@ -11,67 +11,67 @@ interface PositionData{
 export class PositionFunctions{
     static Idle(coords: Coordinates, dir: DIRECTION, t: any): PositionData{
         return {
-            coords: { x: 0, y: 0 },
+            coords: coords,
             endBehavior: { time: Infinity, newState: null }
         }
     }
     static Running(coords: Coordinates, dir: DIRECTION, t: any): PositionData{
         return {
-            coords: { x: 0, y: 0 },
+            coords: coords,
             endBehavior: { time: Infinity, newState: null }
         }
     }
     static Jumping(coords: Coordinates, dir: DIRECTION, t: any): PositionData{
         return {
-            coords: { x: 0, y: 0 },
+            coords: coords,
             endBehavior: { time: Infinity, newState: null }
         }
     }
     static Spiking(coords: Coordinates, dir: DIRECTION, t: any): PositionData{
         return {
-            coords: { x: 0, y: 0 },
+            coords: coords,
             endBehavior: { time: Infinity, newState: null }
         }
     }
     static Passing(coords: Coordinates, dir: DIRECTION, t: any): PositionData{
         return {
-            coords: { x: 0, y: 0 },
+            coords: coords,
             endBehavior: { time: Infinity, newState: null }
         }
     }
     static Setting(coords: Coordinates, dir: DIRECTION, t: any): PositionData{
         return {
-            coords: { x: 0, y: 0 },
+            coords: coords,
             endBehavior: { time: Infinity, newState: null }
         }
     }
     static Blocking(coords: Coordinates, dir: DIRECTION, t: any): PositionData{
         return {
-            coords: { x: 0, y: 0 },
+            coords: coords,
             endBehavior: { time: Infinity, newState: null }
         }
     }
     static Stepback(coords: Coordinates, dir: DIRECTION, t: any): PositionData{
         return {
-            coords: { x: 0, y: 0 },
+            coords: coords,
             endBehavior: { time: Infinity, newState: null }
         }
     }
     static Falling(coords: Coordinates, dir: DIRECTION, t: any): PositionData{
         return {
-            coords: { x: 0, y: 0 },
+            coords: coords,
             endBehavior: { time: Infinity, newState: null }
         }
     }
     static Diving(coords: Coordinates, dir: DIRECTION, t: any): PositionData{
         return {
-            coords: { x: 0, y: 0 },
+            coords: coords,
             endBehavior: { time: Infinity, newState: null }
         }
     }
     static JumpServeTossing(coords: Coordinates, dir: DIRECTION, t: any): PositionData{
         return {
-            coords: { x: 0, y: 0 },
+            coords: coords,
             endBehavior: { time: Infinity, newState: null }
         }
     }
